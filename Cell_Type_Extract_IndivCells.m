5c94592ee2cd37e56219f7da0ab2f87912b0a2b3
justin-torok <jlt46@cornell.edu>
2020-07-13 14:50:00 -0700

    Updates to nG_ParameterFitter.m and others to accommodate SVM

---
+++ Cell_Type_Extract_IndivCells.m
@@ -0,0 +1,94 @@
+function [C_indivcells, classkey, entrez_names, ct_labvec] = Cell_Type_Extract_IndivCells(classstruct,directory)
+
+if nargin < 2
+    directory = [cd filesep 'MatFiles'];
+end
+
+load([directory filesep 'ISH_data.mat'],'GENname','wherecoronal');
+load([directory filesep 'mouse_VISp_2018-06-14_genes-rows.mat'],'filestruct'); 
+genesrows = filestruct;
+
+uni_wb_genenames = unique(GENname(wherecoronal));
+genebool = ismember(genesrows.gene_symbol,uni_wb_genenames);
+ct_entrez = genesrows.gene_entrez_id(genebool);
+entrezbool = ismember(classstruct.VISp.EntrezID,ct_entrez);
+entrez_names = genesrows.gene_symbol(entrezbool);
+members = ismember(filestruct.gene_symbol,entrez_names);
+
+C_indivcells = [];
+regnames = fieldnames(classstruct);
+sublabs = {};
+typelabs = {};
+
+for i = 1:length(regnames)
+    classnames = fieldnames(classstruct.(regnames{i}));
+    exclinds = ismember(classnames,{'EntrezID','Outlier','Low_Quality'});
+    classnames(exclinds) = [];
+    for j = 1:length(classnames)
+        typenames = fieldnames(classstruct.(regnames{i}).(classnames{j}));
+        typenames([1 end-1:end]) = [];
+        for k = 1:length(typenames)
+            curtype = typenames{k};
+            subnames = fieldnames(classstruct.(regnames{i}).(classnames{j}).(typenames{k}));
+            subnames([1 end-1:end]) = [];
+            for h = 1:length(subnames)
+                C_indivcells = [C_indivcells classstruct.(regnames{i}).(classnames{j}).(typenames{k}).(subnames{h}).normalizedexpr(members,:)];
+                cursublabs = cell(1,classstruct.(regnames{i}).(classnames{j}).(typenames{k}).(subnames{h}).cellcount);
+                cursub = subnames{h};
+                cursublabs(:) = {cursub};
+                sublabs = [sublabs cursublabs];
+                curtypelabs = cell(size(cursublabs));
+                curtypelabs(:) = {curtype};
+                typelabs = [typelabs curtypelabs];
+            end
+        end
+    end
+end
+
+for i = 1:length(typelabs)
+    curtype = typelabs{i};
+    if strcmp(curtype,'Olig1')
+        typelabs{i} = 'Oligo';
+    elseif strcmp(curtype,'OPC')
+        typelabs{i} = 'Oligo';
+    elseif strcmp(curtype,'Macrophage')
+        typelabs{i} = 'Macro';
+    elseif strcmp(curtype,'Micro')
+        typelabs{i} = 'Macro';
+    elseif strcmp(curtype,'Peri')
+        typelabs{i} = 'Endo';
+    elseif strcmp(curtype,'SMC')
+        typelabs{i} = 'Endo';
+    end
+end
+
+load([directory filesep 'PresetInputs.mat'],'classkey');
+ct_labvec = zeros(size(typelabs));
+subt_labvec = zeros(size(sublabs));
+dex = 1;
+for i = 1:length(classkey)
+    membs = ismember(typelabs,classkey{i});
+    ct_labvec(membs) = i;
+    cursubkeys = unique(sublabs(membs));
+    for j = 1:length(cursubkeys)
+        submembs = ismember(sublabs,cursubkeys{j});
+        subt_labvec(submembs) = dex;
+        dex = dex + 1;
+    end 
+end
+
+excl_type = 0;
+
+excltype_inds = (ct_labvec==excl_type);
+C_indivcells(:,excltype_inds) = [];
+ct_labvec(excltype_inds) = [];
+subt_labvec(excltype_inds) = [];
+typelabs(excltype_inds) = [];
+if sum(excl_type) > 0
+    classkey(excl_type) = [];
+end
+
+clearvars -except C_indivcells entrez_names classkey ct_labvec
+
+end
+
--- lambda_ParameterFitter.m
+++ lambda_ParameterFitter.m
@@ -1,4 +1,4 @@
-function [lambdastruct, peakind] = lambda_ParameterFitter(voxvgene, genevct, gene_names, ng_param_list, lambda_param_list)
+function [lambdastruct, peakind] = lambda_ParameterFitter(voxvgene, genevct, gene_names, ng_param_list, lambda_param_list, costfun, k, C_indivcells, ct_labvec, matdir)
 % This function performs a brute-force parameter sweep to find the maximal
 % performance of a given subset selection method. The user MUST supply
 % voxvgene and genevct, which are outputs of ProcessedData_Generator.m, and
@@ -12,30 +12,49 @@ function [lambdastruct, peakind] = lambda_ParameterFitter(voxvgene, genevct, gen
 % different aspects of the data processing and quantitative assessment. 
 
 % Default lambda and nG parameter lists
-if nargin < 5
-    lambda_param_list = 50:50:500;
-    if nargin < 4
-        ng_param_list = 100:70:800;
-        if nargin < 3
-            error(sprintf('Error. \nUser must supply a voxel x gene matrix, a gene x cell type matrix, and a subsetting method'))
+if nargin < 10
+    matdir = cd;
+    if nargin < 9
+        ct_labvec = [];
+        if nargin < 8
+            C_indivcells = [];
+            if nargin < 7
+                k = 4;
+                if nargin < 6
+                    costfun = 'SumFit';
+                    if nargin < 5
+                        lambda_param_list = 50:50:500;
+                        if nargin < 4
+                            ng_param_list = 100:70:800;
+                            if nargin < 3
+                                error(sprintf('Error. \nUser must supply a voxel x gene matrix, a gene x cell type matrix, and a subsetting method'))
+                            end
+                        end
+                    end 
+                end
+            end
         end
     end
-end 
+end
 
-sumfit_mat = zeros(length(ng_param_list),length(lambda_param_list));
+cost_mat = zeros(length(ng_param_list),length(lambda_param_list));
 for i = 1:length(lambda_param_list)
     fprintf('Running nG_ParameterFitter, lambda value %d/%d\n',i,length(lambda_param_list))
     lam = lambda_param_list(i);
-    outstruct = nG_ParameterFitter(voxvgene,genevct,gene_names,'MRx3',ng_param_list,lam);
+    outstruct = nG_ParameterFitter(voxvgene,genevct,gene_names,'MRx3',ng_param_list,lam,costfun,k,C_indivcells,ct_labvec,matdir);
     if i == 1
         lambdastruct = outstruct;
     else
         lambdastruct = cat(2,lambdastruct,outstruct);
     end
     for j = 1:length(ng_param_list)
-        sumfit_mat(j,i) = outstruct(j).sumfit;
+        if strcmp(costfun,'SumFit')
+            cost_mat(j,i) = -outstruct(j).sumfit;
+        else
+            cost_mat(j,i) = outstruct(j).error;
+        end
     end
     fprintf('Done, lambda value %d/%d\n',i,length(lambda_param_list))
 end
-[~,peakind] = max(sumfit_mat(:));
+[~,peakind] = min(cost_mat(:));
 end
